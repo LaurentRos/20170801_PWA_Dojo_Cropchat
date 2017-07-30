a2f5a2ea90803a2fa61560e02e5561c9037f4f23
Laurent Ros <laurentr@theodo.fr>
2017-07-30 14:16:48 +0200

    Remove actions and content

--- cropchat/src/components/Card.vue
+++ cropchat/src/components/Card.vue
@@ -8,20 +8,18 @@
       <div class="md-title">Title goes here</div>
       <div class="md-subhead">Subtitle here</div>
     </md-card-header>
-
-    <md-card-actions>
-      <md-button>Action</md-button>
-      <md-button>Action</md-button>
-    </md-card-actions>
-
-    <md-card-content>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-    </md-card-content>
   </md-card>
 </template>
 
 <script>
-
+  export default {
+    props: {
+      imageUrl: String,
+      imageAltText: String,
+      imageTitle: String,
+      imageSubtitle: String
+    }
+  }
 </script>
 
 <style>
